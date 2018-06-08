74d1641e23ef38e72492e188382fef2b0b621133
NamHyuk <rlaskawk1234@gmail.com>
2018-06-08 17:40:44 +0900

    3번째 커밋

--- app/src/main/java/com/example/user/supervise_2nd_heart/util/Constants.java
+++ app/src/main/java/com/example/user/supervise_2nd_heart/util/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     public static final String[] LIST_MENU = {"★ 예체능관련분과", "동아리List2", "동아리List3", "동아리List4", "동아리List5",
             "★ 문학관련분과", "동아리List7", "동아리List8", "동아리List9", "동아리List10", "★ 봉사분과", "동아리List12", "동아리List13",
             "동아리List14", "동아리List15"};
+
+    int inint=0;
 }
+
