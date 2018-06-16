26de4dcf350be28c385f6d1f95db7061ee790487
NamHyuk <rlaskawk1234@gmail.com>
2018-06-16 18:16:40 +0900

    3번째 커밋

--- app/src/main/java/com/example/user/supervise_2nd_heart/admin/admin_customer.java
+++ app/src/main/java/com/example/user/supervise_2nd_heart/admin/admin_customer.java
@@ -1,7 +1,7 @@
 package com.example.user.supervise_2nd_heart.admin;
 
-import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,18 +10,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.content.Intent;
 
 import com.example.user.supervise_2nd_heart.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.util.Map.Entry;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -30,6 +32,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class admin_customer extends Fragment {
     private static String TAG = "phptest_MainActivity";
@@ -45,6 +48,8 @@ public class admin_customer extends Fragment {
     ListView mListViewList;
     EditText mEditTextSearchKeyword;
     String mJsonString;
+    Button button_search;
+    FragmentManager manager;
 
 
     @Nullable
@@ -56,19 +61,69 @@ public class admin_customer extends Fragment {
         mArrayList = new ArrayList<HashMap<String, String>>();
         mEditTextSearchKeyword = (EditText) v.findViewById(R.id.editText_main_searchKeyword);
 
-        Button button_search = (Button) v.findViewById(R.id.button_main_search);
+        button_search = (Button) v.findViewById(R.id.button_main_search);
+
+        GetData2 task2 = new GetData2();
+        task2.execute("http://211.115.254.166:8282/loadinfo.php");
+
         button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mArrayList.clear();
 
+
                 GetData task = new GetData();
+
                 task.execute(mEditTextSearchKeyword.getText().toString());
-//                task.execute("http://211.115.254.166:8282/loadinfo.php");
+
+                mListViewList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(getActivity(), AdminMoreInfo.class);
+                        if(i==0){
+                            intent.putExtra("info", mEditTextSearchKeyword.getText().toString());
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+
+
             }
         });
+        mListViewList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                for (int s=0;s<mArrayList.size();s++)
+                {
+                    if(i==s)
+                    {
+                       // Log.d(TAG, "s값  - " +mArrayList.get(i));
+
+                        HashMap map = new HashMap();
+                        map=mArrayList.get(i);
+
+                        Iterator<String> iterator = map.keySet().iterator();
+                        while (iterator.hasNext()){
+                            String key = (String) iterator.next();
+                            //Log.i("INFO", "key : "+key);
+                            //Log.i("INFO", "value : ska "+mArrayList.get(i).get(key));
+                            if(key=="userCustomer")
+                            {
+                                //Log.i("INFO", "value : ska "+mArrayList.get(i).get(key));
+                                Intent intent = new Intent(getActivity(), AdminMoreInfo.class);
+                                intent.putExtra("info", mArrayList.get(i).get(key));
+                                startActivity(intent);
+                            }
+                        }
+
+                    }
+                }
 
 
+            }
+        });
         return v;
     }
 
@@ -87,15 +142,10 @@ public class admin_customer extends Fragment {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
             progressDialog.dismiss();
             Log.d(TAG, "response  - " + result);
-
-
             mJsonString = result;
             showResult();
-
-
         }
 
         @Override
@@ -104,13 +154,86 @@ public class admin_customer extends Fragment {
             String searchKeyword = params[0];
             String serverURL = "http://211.115.254.166:8282/userdatasearch.php";
             String postParameters = "customer=" + searchKeyword;
-
-
             try {
 
                 URL url = new URL(serverURL);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+
+    private class GetData2 extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(getActivity(),
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+
+            Log.d(TAG, "response  - " + result);
+
+            mJsonString = result;
+            showResult();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String searchKeyword = params[0];
+            String serverURL = params[0];
+
+            String postParameters = "customer=" + searchKeyword;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
                 httpURLConnection.setReadTimeout(5000);
                 httpURLConnection.setConnectTimeout(5000);
@@ -133,7 +256,6 @@ public class admin_customer extends Fragment {
                     inputStream = httpURLConnection.getErrorStream();
                 }
 
-
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
@@ -143,63 +265,44 @@ public class admin_customer extends Fragment {
                 while ((line = bufferedReader.readLine()) != null) {
                     sb.append(line);
                 }
-
-
                 bufferedReader.close();
-
-
                 return sb.toString().trim();
-
-
             } catch (Exception e) {
 
                 Log.d(TAG, "InsertData: Error ", e);
                 errorString = e.toString();
-
                 return null;
             }
-
         }
     }
 
-    private void showResult() {
 
+    private void showResult() {
         try {
             JSONObject jsonObject = new JSONObject(mJsonString);
             JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
-
             for (int i = 0; i < jsonArray.length(); i++) {
-
                 JSONObject item = jsonArray.getJSONObject(i);
-
                 String id = item.getString(TAG_ID);
                 String password = item.getString(TAG_PASSWORD);
                 String customer = item.getString(TAG_CUSTOMER);
-
 
                 HashMap<String, String> hashMap = new HashMap<>();
 
                 hashMap.put(TAG_ID, id);
                 hashMap.put(TAG_PASSWORD, password);
                 hashMap.put(TAG_CUSTOMER, customer);
-
-
+                //System.out.println(id + "\t" + password + "\t" + customer);
                 mArrayList.add(hashMap);
             }
-
             ListAdapter ListAdapter = new SimpleAdapter(
                     getActivity(), mArrayList, R.layout.item_list,
                     new String[]{TAG_ID, TAG_PASSWORD, TAG_CUSTOMER},
                     new int[]{R.id.textView_list_id, R.id.textView_list_password, R.id.textView_list_Customer}
             );
             mListViewList.setAdapter(ListAdapter);
-
         } catch (JSONException e) {
-
             Log.d(TAG, "showResult : ", e);
         }
-
     }
-
 }
-
